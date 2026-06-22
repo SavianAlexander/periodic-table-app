510b25c5e7668a0f3a4169025d4f6751746a7bae
Roberto Morales <roberto.morales@example.com>
2026-06-22 19:54:02 -0400

    Configure GitHub Pages: Add deploy.yml CI workflow and conditional base path in vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -2,9 +2,13 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  server: {
-    host: '127.0.0.1',
-  },
+export default defineConfig(({ command }) => {
+  return {
+    plugins: [react()],
+    base: command === 'build' ? '/periodic-table-app/' : '/',
+    server: {
+      host: '127.0.0.1',
+    },
+  }
 })
+
